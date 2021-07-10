b3d352386469264c72140bb22512362bd7c1f07c
Victor Xu <VanXNF@gmail.com>
2021-07-10 14:21:42 +0800

    1. finish Homework 4

--- hw_4.py
+++ hw_4.py
@@ -1,119 +1,154 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import numpy as np
-import operator
 import matplotlib.pyplot as plt
+from collections import Counter
 
 
 def load_dataset(dataset_path):
+    """
+    加载数据集\n
+    :param dataset_path: 数据集路径
+    :return:
+    """
     data_frame = pd.read_excel(dataset_path, sheet_name='Data')
-    data_class = {}
-    dataset_test = []
-    label_test = []
-    dataset_train = []
-    label_train = []
+    datasets = []
+    labels = []
     for i, data in data_frame.iterrows():
-        if data_class.get(f"{data[1]}") is None:
-            data_class[f"{data[1]}"] = 1
-            dataset_test.append(list(data[2:]))
-            label_test.append(data[1])
-        else:
-            if data_class.get(f"{data[1]}") < 2:
-                dataset_test.append(list(data[2:]))
-                label_test.append(data[1])
-                data_class[f"{data[1]}"] += 1
-            else:
-                dataset_train.append(list(data[2:]))
-                label_train.append(data[1])
+        datasets.append(list(data[2:]))
+        labels.append(data[1])
     columns_name = data_frame.columns.values.tolist()[2:]
-    return dataset_train, label_train, dataset_test, label_test, columns_name
+    return datasets, labels, columns_name
 
 
-# 定义KNN算法分类器函数
-# 函数参数包括：(测试数据，训练数据，分类,k值)
-def knn_classify(data_test, dataset_train, label_train, k):
-    dataset_size = len(dataset_train)
-    # 将 测试数据
-    diffMat = np.tile(data_test, (dataset_size, 1)) - dataset_train
+class KMeans(object):
+    def __init__(self, k=6, tolerance=0.0001, max_iter=300):
+        self.k = k  # 聚簇数量
+        self.tolerance = tolerance  # 中心点误差
+        self.max_iter = max_iter  # 最大迭代次数
+        self.centers = {}  # 簇中心点
+        self.clf = {}  # 聚簇分类字典
 
-    sqDiffMat = diffMat ** 2
-    sqDistances = sqDiffMat.sum(axis=1)
-    distances = sqDistances ** 0.5  # 计算欧式距离
-    sortedDistIndicies = distances.argsort()  # 排序并返回index
-    # 选择距离最近的k个值
-    classCount = {}
-    for i in range(k):
-        voteIlabel = label_train[sortedDistIndicies[i]]
-        # D.get(k[,d]) -> D[k] if k in D, else d. d defaults to None.
-        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
-    # 排序
-    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
-    return sortedClassCount[0][0]
+    def fit(self, train_data):
+        self.centers = {}
+        # 取前 k 个作为初始均值向量
+        for i in range(self.k):
+            self.centers[i] = train_data[i]
+        # 迭代求解
+        for i in range(self.max_iter):
+            self.clf = {}
+            for j in range(self.k):
+                self.clf[j] = []
+            for feature in train_data:
+                distances = []
+                for center in self.centers:
+                    # 欧拉距离
+                    distances.append(np.linalg.norm(feature - self.centers[center]))
+                classification = distances.index(min(distances))
+                self.clf[classification].append(feature)
 
+            # 更新均值向量
+            prev_centers = dict(self.centers)
+            for c in self.clf:
+                self.centers[c] = np.average(self.clf[c], axis=0)
 
-class KNearestNeighbor(object):
-    def __init__(self):
-        pass
+            # 检测簇心是否不再变动
+            optimized = True
+            for center in self.centers:
+                org_centers = prev_centers[center]
+                cur_centers = self.centers[center]
+                if np.sum((cur_centers - org_centers) / org_centers * 100.0) > self.tolerance:
+                    optimized = False
+            # 已达到最优则退出
+            if optimized:
+                break
 
-    # 训练函数
-    def train(self, X, y):
-        self.X_train = X
-        self.y_train = y
-
-    # 预测函数
-    def predict(self, X, k=1):
-        # 计算L2距离
-        num_test = X.shape[0]
-        num_train = self.X_train.shape[0]
-        dists = np.zeros((num_test, num_train))  # 初始化距离函数
-        # because(X - X_train)*(X - X_train) = -2X*X_train + X*X + X_train*X_train, so
-        d1 = -2 * np.dot(X, self.X_train.T)  # shape (num_test, num_train)
-        d2 = np.sum(np.square(X), axis=1, keepdims=True)  # shape (num_test, 1)
-        d3 = np.sum(np.square(self.X_train), axis=1)  # shape (1, num_train)
-        dist = np.sqrt(d1 + d2 + d3)
-        # 根据K值，选择最可能属于的类别
-        y_pred = np.zeros(num_test)
-        for i in range(num_test):
-            dist_k_min = np.argsort(dist[i])[:k]  # 最近邻k个实例位置
-            y_kclose = self.y_train[dist_k_min]  # 最近邻k个实例对应的标签
-            y_pred[i] = np.argmax(np.bincount(y_kclose.tolist()))  # 找出k个标签中从属类别最多的作为预测类别
-
-        return y_pred
+    def predict(self, test_data):
+        distances = [np.linalg.norm(test_data - self.centers[center]) for center in self.centers]
+        index = distances.index(min(distances))
+        return index
 
 
-# 定义一个生成“训练样本集”的函数，包含特征和分类信息
-def createDataSet():
-    group = [[1, 1.1], [1, 1], [0, 0], [0, 0.1]]
-    labels = ['A', 'A', 'B', 'B']
-    return group, labels
-
-
-def draw_fig(columns_name):
+def draw_fig(first_index, second_index, class_dict, k, columns_name):
+    """
+    绘制散点图\n
+    :param first_index: 属性 1 位置
+    :param second_index: 属性 2 位置
+    :param class_dict: 类别字典
+    :param k: 类别
+    :param columns_name: 属性名
+    :return:
+    """
+    color_list = ['b', 'g', 'r', 'c', 'm', 'y']
+    marker_list = ['o', '*', '.', 'x', '+', 's']
     plt.clf()
-    plt.title(f"KNN RESULT ({name_columns[first_index]},{name_columns[second_index]})")
-    plt.scatter([i[first_index] for i in train_dataset], [i[second_index] for i in train_dataset], color='red',
-                marker='o',
-                label='setosa')
-    # plt.scatter(X_versicolor[:, 0], X_versicolor[:, 2], color='blue', marker='^', label='versicolor')
-    # plt.scatter(X_virginica[:, 0], X_virginica[:, 2], color='green', marker='s', label='virginica')
-    plt.xlabel(f'{name_columns[first_index]}')
-    plt.ylabel(f'{name_columns[second_index]}')
-    plt.legend(loc='upper left')
+    plt.title(f"K Means RESULT ({columns_name[first_index]},{columns_name[second_index]})")
+    for index in range(k):
+        dataset = class_dict.get(index)
+        plt.scatter([i[0] for i in dataset], [i[1] for i in dataset],
+                    color=f"{color_list[index]}",
+                    marker=f"{marker_list[index]}",
+                    label=index)
+
+    plt.xlabel(f'{columns_name[first_index]}')
+    plt.ylabel(f'{columns_name[second_index]}')
+    plt.legend()
     # plt.show()
     plt.savefig(f"./hw4/{first_index}_{second_index}.png")
 
 
+def cal_accuracy(datasets, labels, model):
+    class_dict = {}
+    for index in range(6):
+        class_dict[index] = []
+    for index in range(len(labels)):
+        label = labels[index]
+        if label == 'car':
+            dict_index = 0
+        elif label == 'fad':
+            dict_index = 1
+        elif label == 'mas':
+            dict_index = 2
+        elif label == 'gla':
+            dict_index = 3
+        elif label == 'con':
+            dict_index = 4
+        else:
+            # label = 'adi'
+            dict_index = 5
+        class_dict[dict_index].append(model.predict(np.array(datasets[index])))
+    accuracy = 0
+    for index in range(6):
+        data_list = class_dict.get(index)
+        # 获取该类别中数量最多的类别个数，即类别正确率
+        correct_num = Counter(data_list).most_common(1)[0][1]
+        # 加权计算
+        accuracy += len(data_list) * (correct_num / len(data_list))
+    accuracy = accuracy / len(labels)
+    return accuracy
+
+
 if __name__ == "__main__":
     dataset_file = "./datasets/BreastTissue/BreastTissue.xls"
-    train_dataset, train_label, test_dataset, test_label, name_columns = load_dataset(dataset_file)
+    datasets, labels, name_columns = load_dataset(dataset_file)
 
+    best_accuracy = 0
+    best_first_index = 0
+    best_second_index = 0
     for first_index in range(9):
         for second_index in range(9):
             if first_index == second_index:
                 continue
+            k_means = KMeans(k=6)
+            sub_dataset = [[i[first_index], i[second_index]] for i in datasets]
+            k_means.fit(np.array(sub_dataset))
+            accuracy = cal_accuracy(sub_dataset, labels, k_means)
+            if accuracy > best_accuracy:
+                best_accuracy = accuracy
+                best_first_index = first_index
+                best_second_index = second_index
+                print(accuracy)
+                draw_fig(first_index, second_index, k_means.clf, 6, name_columns)
 
-    # print(np.tile([12, 13], (2, 3)))
-    # group, labels = createDataSet()
-    # # 对测试数据[0,0]进行KNN算法分类测试
-    # s = knn_classify([0, 0], group, labels, 3)
-    # print(s)
+    print(f"best accuracy: {best_accuracy}, "
+          f"using attribute {name_columns[best_first_index]} and {name_columns[best_second_index]}")
